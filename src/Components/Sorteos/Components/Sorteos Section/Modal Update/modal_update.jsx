226035aa4a2b772941f6144fd7e5c724f2335f1e
nicosorteos <160674060+nicosorteos@users.noreply.github.com>
2024-02-20 17:01:10 -0600

    Update modal_update.jsx

--- src/Components/Sorteos/Components/Sorteos Section/Modal Update/modal_update.jsx
+++ src/Components/Sorteos/Components/Sorteos Section/Modal Update/modal_update.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Modal.scss";
+import "./modal.scss";
 import axios from "axios";
 
 const ModalUpdate = ({ isOpen, onClose, editId }) => {
